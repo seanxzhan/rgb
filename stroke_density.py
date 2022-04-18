bd3753161790a9eee4eed597e6c6c754e45ae7fc
Sean Zhan <seanzhan0319@hotmail.com>
2022-04-18 00:35:57 -0400

    stroke density matching paper src code

--- stroke_density.py
+++ stroke_density.py
@@ -40,6 +40,10 @@ def getBarycenter(hull):
     return total
 
 
+def getMean(points):
+    return np.mean(points, axis=0)
+
+
 def getRayDirs(barycenter, points):
     diff = points - barycenter
     factor = np.linalg.norm(diff, axis=1, keepdims=True)
@@ -52,6 +56,7 @@ def getStrokeDensity(barycenter, ray_d, hull, loc_out_path):
     ray_p = np.repeat(np.reshape(barycenter, (1,-1)), axis=0, repeats=num_rays)
     mesh = trimesh.Trimesh(vertices=hull.points, faces=hull.simplices)
     if os.path.exists(loc_out_path):
+    # if False:
         print("loading intersection...")
         f = np.load(loc_out_path, allow_pickle=True)
         loc = f['loc']
@@ -68,10 +73,11 @@ def getStrokeDensity(barycenter, ray_d, hull, loc_out_path):
     for i in range(loc.shape[0]):
         new_loc[ray_idx[i]] = loc[i]
     loc = new_loc
-    numerator = np.linalg.norm(hull.points - loc, axis=1, keepdims=True)
+    # numerator = np.linalg.norm(hull.points - loc, axis=1, keepdims=True)
+    numerator = np.linalg.norm(hull.points - ray_p, axis=1, keepdims=True)
     denominator = np.linalg.norm(ray_p - loc, axis=1, keepdims=True)
     K = numerator / denominator
-    K = np.abs(1 - K)
+    K *= 1.2
     K = np.clip(K, 0, 1) * 255
     return K
 
@@ -86,17 +92,24 @@ def save_stroke_density_as_img(stroke_density, h, w, out_path):
 # run the stroke density algorithm
 def main():
     VIS = False
+    USE_BARYCENTER = False
 
     points, H, W = getPoints("./data/sample-input.png")
+    # points, H, W = getPoints("./data/001.jpg")
     hull = getHull(points)
     if VIS:
         plt = visualize.show_convex_hull(hull)
         plt.savefig("./data/convex_hull_vis.png", bbox_inches='tight', pad_inches=0)
         plt.show()
         plt.close()
-    barycenter = getBarycenter(hull)
-    ray_dirs = getRayDirs(barycenter, points)
-    stroke_density = getStrokeDensity(barycenter, ray_dirs, hull, "./data/intersection.npz")
+    
+    if USE_BARYCENTER:
+        center = getBarycenter(hull)
+    else:
+        center = getMean(points)
+
+    ray_dirs = getRayDirs(center, points)
+    stroke_density = getStrokeDensity(center, ray_dirs, hull, "./data/intersection.npz")
     save_stroke_density_as_img(stroke_density, H, W, "./data/stroke_density.png")
 
 
