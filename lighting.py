558fe45ea089700b782adb206c2143af1d218b7a
Marc Mapeke <marc_mapeke@brown.edu>
2022-04-19 23:56:34 -0400

    very strong attempt at coarse lighting, still bugs

--- lighting.py
+++ lighting.py
@@ -73,19 +73,19 @@ def computeCoarseLightingEffect(N):
     # TODO: Vectorize this
    
     # Top right of image
-    l_x = 480
-    l_y = 0
+    orig_l_x = l_x = 650
+    orig_l_y = l_y = 50
     l_z = 1
 
     height, width, _  = N.shape
     print(width, height)
    
-    delta = float(1.0)
-
-    # p_x, p_y = np.meshgrid(np.arange(width), np.arange(height))
-    # p_d = np.sqrt((p_x - l_x)**2 + (p_y - l_y)**2)
+    # Is this a relative or absolute value??
+    # delta = 2 * float(1.0) / float(width) # This is 1 pixel in [-1, 1] x [-1, 1]
+    delta = 2 * 50 * float(1.0) / float(width) # This works much better 
 
     # Normalizing light/mouse location to [-1, 1] x [-1, 1]
+    # I think l_z = 1 is fine??
     l_x = (2 * l_x / float(width)) - 1
     l_y = (2 * l_y / float(height)) - 1
     print(l_x, l_y)
@@ -96,101 +96,93 @@ def computeCoarseLightingEffect(N):
     sin_theta = np.divide(p_y - l_y, p_d)
     cos_theta = np.divide(p_x - l_x, p_d)
 
-
     # If Light is above image, need to replace sin and cos at point
     # Exact direction shouldn't matter, because infinite choices
-    # Other sin and cos will be inf or cause nan later on
-    # if (l_x >= 0 and l_x < width and  l_y >= 0 and l_y < height):
-    #     sin_theta[l_y, l_x] = 0
-    #     cos_theta[l_y, l_x] = 1
-    
-
-    orig_l_x = 480
-    orig_l_y = 0
+    # Otherwise, sin and cos will be inf or cause nan later on
     if (orig_l_x >= 0 and orig_l_x < width and  orig_l_y >= 0 and orig_l_y < height):
         sin_theta[orig_l_y, orig_l_x] = 0
         cos_theta[orig_l_y, orig_l_x] = 1
-
-    # print(sin_theta[l_y])
-    # print(cos_theta[l_y])
 
     E = np.empty(N.shape)
     for y in range(height):
         for x in range(width):
             for c in range(3):
                 light_direction = np.array([l_z, p_d[y, x]])
-                # print(light_direction)
                 light_direction = light_direction / np.sqrt(np.sum(light_direction**2))
-                # print(light_direction)
                 
-                # Represent origin point on N and interpolated point offset by delta
-                # n1 = N[
-                #     round((l_y + sin_theta[y, x] * p_d[y, x])),
-                #     round((l_x + cos_theta[y, x] * p_d[y, x])), 
-                #     c]
-
-                # x_interp = l_x + cos_theta[y, x] * (p_d[y, x] + delta)
-                # y_interp = l_y + sin_theta[y, x] * (p_d[y, x] + delta) 
-                
+                # n1 - Represents origin point on N 
                 n1 = N[
                     round(((l_y + sin_theta[y, x] * p_d[y, x]) + 1) * height / float(2)),
                     round(((l_x + cos_theta[y, x] * p_d[y, x]) + 1) * width / float(2)), 
                     c]
 
-                assert(abs(round(((l_y + sin_theta[y, x] * p_d[y, x]) + 1) * height / float(2)) - y) < 1e3)
-                assert(abs(round(((l_x + cos_theta[y, x] * p_d[y, x]) + 1) * width / float(2)) - x) < 1e3)
+                assert(abs(((l_y + sin_theta[y, x] * p_d[y, x]) + 1) * height / float(2) - y) < 1e3)
+                assert(abs(((l_x + cos_theta[y, x] * p_d[y, x]) + 1) * width / float(2) - x) < 1e3)
 
                 x_interp = ((l_x + cos_theta[y, x] * (p_d[y, x] + delta)) + 1) * width / float(2)
                 y_interp = ((l_y + sin_theta[y, x] * (p_d[y, x] + delta)) + 1) * height / float(2)
 
+                # n2 - Interpolated point on N
                 n2 = bilinear_interpolate(N[:, :, c], x_interp, y_interp, width, height)
-                
 
                 wave_direction = np.array([n2 - n1, delta])
-                # print(wave_direction)
                 wave_direction = wave_direction / np.sqrt(np.sum(wave_direction**2))
-                # print(wave_direction)
-
-                # if (y == l_y and x == 250):
-                #                 print(x_interp, y_interp)
-                #                 print(n1)
-                #                 print(n2)
-                #                 print(p_d[y, x])
-                #                 print(l_z)
-                #                 print(light_direction)
-                #                 print(wave_direction)
 
                 E[y, x, c] = np.dot(wave_direction, light_direction)
-                # if ((E[y, x, c] < 0)):
-                    # print("bad1", E[y, x, c])
-                    # exit()
-                # if ((E[y, x, c] > 1)):
-                    # print("bad2", E[y, x, c])
-                    # exit()
-                # if (x == 0 and y == 0):
-                #     print(n1)
-                #     print(n2)
-                #     print(N[0:3, 0:3, 0])
+                # if (E[y, x, c] < 0):
+                #     print("bad")
                 #     print(E[y, x, c])
-                # exit()
 
     E = np.clip(E, 0, 1)
     return E
 
 # refined lighting effect
 
+# Pads array to desired shape with pad_value
+def to_shape(a, shape, pad_value):
+    y_, x_ = shape
+    y, x = a.shape
+    y_pad = (y_-y)
+    x_pad = (x_-x)
+    return np.pad(a,((y_pad//2, y_pad//2 + y_pad%2), 
+                     (x_pad//2, x_pad//2 + x_pad%2)),
+                  mode = 'constant',
+                  constant_values = pad_value)
+
+# Pads image (using background) to be square
+def pad_image(img):
+    
+    # Assuming corner is background for our padding colors for now
+    padding_value_0 = img[0, 0, 0] 
+    padding_value_1 = img[0, 0, 1] 
+    padding_value_2 = img[0, 0, 2] 
+
+    max_side_length = max(img.shape[0], img.shape[1])
+    padded_img = np.stack((
+        to_shape(img[:, :, 0], (max_side_length, max_side_length), padding_value_0),
+        to_shape(img[:, :, 1], (max_side_length, max_side_length), padding_value_1),
+        to_shape(img[:, :, 2], (max_side_length, max_side_length), padding_value_2)
+    ), axis = 2)
+
+    return padded_img
+
 def main():
     print("Testing lighting.py")
     img = cv2.imread("./data/sample-input.png", cv2.IMREAD_COLOR)
     
-    N = computeNormalizedChannelIntensity(img)
+    padded_img = pad_image(img)
+
+    N = computeNormalizedChannelIntensity(padded_img)
     cv2.imwrite("./data/normalized-channels.png", (N * 255).astype(np.ubyte))
 
-    N = cv2.imread("./data/sample-N.png", cv2.IMREAD_COLOR)
-    N = N / float(255) 
+    # Uses saved N image from paper screenshot
+    # N = cv2.imread("./data/sample-N.png", cv2.IMREAD_COLOR)
+    # N = pad_image(N)
+    # N = N / float(255) 
 
     E = computeCoarseLightingEffect(N)
-    cv2.imwrite("./data/coarse-lighting-effect-normalized.png", (E * 255).astype(np.ubyte))
+    print(E.min(), E.max())
+    cv2.imwrite("./data/E.png", (E * 255).astype(np.ubyte))
 
 if __name__ == "__main__":
     main()
