2fcba7cd96407c08fbe8934b76a6384a44f2d946
Sean Zhan <seanzhan0319@hotmail.com>
2022-05-12 00:45:33 -0400

    added specular

--- main_specular.py
+++ main_specular.py
@@ -3,48 +3,48 @@ import re
 import cv2
 import numpy as np
 from PIL import Image
-import stroke_density, lighting, lighting_vectorized
-from lighting_specular import resize_and_pad
+import stroke_density, lighting_specular
 
 
-def main(input_path, results_dir, scale_percent, which_corner):
+def main(input_path, depth_in_path, results_dir,
+         light_which_corner, eye_which_corner):
     raw_img_path = input_path
-    output_path = os.path.join(results_dir, "rendered"+str(which_corner)+".png")
+    output_path = os.path.join(results_dir, "rendered"+str(eye_which_corner)+".png")
     K_out = os.path.join(results_dir, "K.png")
     intersect_path = os.path.join(results_dir, "intersections.npz")
     N_out = os.path.join(results_dir, "N.png")
-    E_out = os.path.join(results_dir, "E" + str(which_corner) + ".png")
-    EK_out = os.path.join(results_dir, "EK" + str(which_corner) + ".png")
-    S_out = os.path.join(results_dir, "S" + str(which_corner) + ".png")
+    E_out = os.path.join(results_dir, "E" + str(light_which_corner) + ".png")
+    EK_out = os.path.join(results_dir, "EK" + str(light_which_corner) + ".png")
+    S_out = os.path.join(results_dir, "S" + str(light_which_corner) + ".png")
+    SS_out = os.path.join(results_dir, "S" + str(light_which_corner) + str(eye_which_corner) + ".png")
+    blurred_depth_out = os.path.join(results_dir, "blurred_depth.png")
+    norm_out = os.path.join(results_dir, "normals.png")
+    specular_out = os.path.join(results_dir, "spec" + str(eye_which_corner) + ".png")
 
     ambient_intensity = 0
     light_intensity = 1
 
     # original image
     R = cv2.imread(raw_img_path, cv2.IMREAD_COLOR)
-    print("original shape: {}, {}".format(R.shape[1], R.shape[0]))
-    width = int(R.shape[1] * scale_percent / 100)
-    height = int(R.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    R = cv2.resize(R, dim, interpolation = cv2.INTER_AREA)
-    # R = resize_and_pad(R)
-    print("resized shape: {}, {}".format(width, height))
-    
+    R = lighting_specular.resize_and_pad(R)
+
     # stroke density
     K = stroke_density.get_stroke_density(R, intersect_path)
     cv2.imwrite(K_out, (K * 255).astype(np.ubyte))
 
-    # normalized channel and coarse lighting
-    desaturate = True
-    saturation_factor = 1.0 # Only relevant if desaturate = True
-    # N, E = lighting.get_lighting(R, which_corner, desaturate, saturation_factor) # 0~1
-    N, E = lighting_vectorized.get_lighting(
-        R, which_corner, desat_coarse=True, coarse_desat_factor=0.5) # 0~1
+    # get depth
+    d_im = cv2.imread(depth_in_path, cv2.IMREAD_GRAYSCALE)
+    d_im = d_im[:, int(d_im.shape[1] / 2):]
 
+    # normalized channel and coarse lighting
+    d_im, normals, N, E, specular =\
+        lighting_specular.get_lighting(R, d_im, light_which_corner, eye_which_corner)
+
+    cv2.imwrite(blurred_depth_out, d_im)
+    cv2.imwrite(norm_out, (normals * 255).astype(np.ubyte))
     cv2.imwrite(N_out, (N * 255).astype(np.ubyte))
     cv2.imwrite(E_out, (E * 255).astype(np.ubyte))
-
-    # TODO: single image normal estimation for specular
+    cv2.imwrite(specular_out, (specular * 255).astype(np.ubyte))
 
     # refinement
     EK = K + np.multiply(E, 1 - K).clip(0, 1)
@@ -52,10 +52,12 @@ def main(input_path, results_dir, scale_percent, which_corner):
     cv2.imwrite(EK_out, (EK * 255).astype(np.ubyte))
 
     # output
-    S = light_intensity * EK + ambient_intensity
+    S = light_intensity * EK + ambient_intensity + specular
+    SS = S.copy()
     S = S.clip(0, 1)
     S = S**1.5
     cv2.imwrite(S_out, (S * 255).astype(np.ubyte))
+
     I = np.multiply(R, S).clip(0, 255)
     cv2.imwrite(output_path, (I).astype(np.ubyte))
 
@@ -81,34 +83,25 @@ def save_images_as_gif(dir_, condition, gif_name):
 
 
 if __name__ == "__main__":
-    scale_dict = {
-        "sample-input.png": 100,
-        "007.jpg": 60,
-        "013.jpg": 100,
-        "018.jpg": 60,
-        "022.jpg": 60,
-        "023.jpg": 30,
-        "028.jpg": 40,
-        "042.jpg": 60,
-    }
+    id = "013"
+    assert id in ["007", "012", "013", "016", "028", "cb"]
 
     data_dir = "./imgs"
+    depth_dir = "./specular"
     all_results_dir = "./results"
-    filename = "013.jpg"
-
-    # scales down the image to make things go faster
-    # manually add key value pair above
-    scale_percent = scale_dict[filename]
+    filename = id+".jpg"
 
     input_path = os.path.join(data_dir, filename)
-    results_dir = os.path.join(all_results_dir, os.path.splitext(filename)[0])
+    depth_input_path = os.path.join(depth_dir, id, "padded_R_depth.jpg")
+    results_dir = os.path.join(all_results_dir, id + "_spclr")
     
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
-    # for all 4 corners
+    # set light to top right
+    # for all 4 eye corners
     for c in range(1, 5):
-        main(input_path, results_dir, scale_percent, c)
+        main(input_path, depth_input_path, results_dir, 1, c)
 
     def condition(f):
         return f[:8] == "rendered"
